91dc6350760326ea0ffeeaa0547d672ac71893fb
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-14 15:58:16 +0600

    addToCart and removeFromCart is done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Cart = () => {
-    return <div></div>;
+const Cart = ({ cart, handleRemoveFromCart }) => {
+    return (
+        <div>
+            <h2>Item selected: {cart.length}</h2>
+            {cart.map((tShirt) => (
+                <p>
+                    {tShirt.name} <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
+                </p>
+            ))}
+        </div>
+    );
 };
 
 export default Cart;
