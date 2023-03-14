e92c798c77ee67b866f564f2c8772b3e29b8bb70
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-14 21:43:04 +0600

    context api is covered

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -4,6 +4,18 @@ import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css';
 
+/**
+ * context api
+ * 1. call createContext with a default value
+ * 2. set a variable of the context with uppercase
+ * 3. Make sure you export the context to use it in other places
+ * 4. Wrap your child content using RingContext.Provider
+ * 5. Provide a value
+ * 6. to consume the context from child component
+ * 7. useContext hook and you will you need to pass the contextName
+ * 8. Make sure you take notes...
+ **/
+
 export const RingContext = React.createContext('Ring');
 
 const Grandpa = () => {
@@ -16,7 +28,7 @@ const Grandpa = () => {
         setHouse(newHouseCount);
     };
     return (
-        <RingContext.Provider value={ornament}>
+        <RingContext.Provider value={[house, setHouse]}>
             <div className="grandpa">
                 <h2>Grandpa</h2>
                 <p>
--- src/components/Special/Special.js
+++ src/components/Special/Special.js
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = () => {
-    const ring = useContext(RingContext);
+    const [house, setHouse] = useContext(RingContext);
     return (
         <div>
             <h5>Special</h5>
             <p>
-                <small>Gift: {ring}</small>
+                <small>Gift: {house}</small>
             </p>
+            <button onClick={() => setHouse(house + 1)}>Buy A House</button>
         </div>
     );
 };
